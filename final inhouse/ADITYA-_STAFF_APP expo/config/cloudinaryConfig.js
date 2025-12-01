deb059fce09b715602fed9503384b7265eb93480
KATHIPALLI MADHU KIRAN <119720465+kathipallimadhukiran@users.noreply.github.com>
2025-12-01 14:06:32 +0530

    Delete final inhouse directory

--- final inhouse/ADITYA-_STAFF_APP expo/config/cloudinaryConfig.js
+++
@@ -1,3 +0,0 @@
-// Replace these values with your actual Cloudinary configuration
-export const CLOUDINARY_CLOUD_NAME = 'dt4gnmw2f';
-export const CLOUDINARY_UPLOAD_PRESET = 'aditya_app_preset'; 
--- final inhouse/ADITYA-_STAFF_APP expo/config/mongoConfig.js
+++
@@ -1,7 +0,0 @@
-export const mongoConfig = {
-  uri: process.env.MONGODB_API_URL,
-  options: {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-}; 
